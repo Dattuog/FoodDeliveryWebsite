383ee4e51932ba7bdfcba9f596f7d1b2cd913b0b
Dattuog <prsds10082003@gmail.com>
2024-06-25 23:23:36 +0530

    create user order page

--- backend/contollers/orderController.js
+++ backend/contollers/orderController.js
@@ -14,7 +14,7 @@ const stripe = new Stripe(process.env.STRIPE_SECRET_KEY)
 
 const placeOrder = async (req,res) => {
 
-         const frontend_url = "http://localhost:5173" ;
+         const frontend_url = "http://localhost:5174" ;
           try {
               // creating the new order
              const newOrder = new orderModel({
@@ -84,5 +84,37 @@ const verifyOrder = async (req,res) => {
   }
 }
 
+// user orders for frontend
+const userOrders = async (req,res) => {
+         try {
+             const orders = await orderModel.find({userId:req.body.userId}) ;
+             res.json({success:true,data:orders})
+         } catch (error) {
+            console.log(error);
+            res.json({success:false,message:"Error"})
+         }
+}
 
-export {placeOrder,verifyOrder}
+// Listing orders for admin panel
+
+const listOrders = async (req,res) => {
+     try {
+         const orders = await orderModel.find({}) ;
+         res.json({success:true,data:orders})
+        
+     } catch (error) {
+         res.json({success:false ,message:"Error"})
+     }
+}
+
+// api for updating order status 
+const updateStatus = async (req,res) => {
+    try {
+        await orderModel.findByIdAndUpdate(req.body.orderId,{status:req.body.status});
+        res.json({success:true,message:"Status Updated"})
+    } catch (error) {
+        console.log(error);
+        res.json({success:false,message:"Error"})
+    }
+}
+export {placeOrder,verifyOrder,userOrders,listOrders,updateStatus}
